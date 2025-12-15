1728a19003ade6e5af3e6abe07b1172efb449851
anisamalang34-cmyk <anisamalang34@gmail.com>
2025-12-15 14:02:29 +0700

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,20 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6O7idnpvsqJ":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  
+var audio = document.getElementByld('bgSongku');
+audio'scr="musikku.mp3";
+audio.load();
+audio.play();
+audio.volume=0.7;
+}
+
